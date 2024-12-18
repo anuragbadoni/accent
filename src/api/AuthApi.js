2f551b690a4e9c88491106bb60d0c831c6ec52ce
anuragbadoni <104890769+anuragbadoni@users.noreply.github.com>
2024-12-18 16:15:49 +0530

    Update AuthApi.js

--- src/api/AuthApi.js
+++ src/api/AuthApi.js
@@ -1,9 +1,9 @@
 //! This file contains all the API calls related to Authentication
 
 import axios from "axios";
-
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: API_BASE_URL,
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
